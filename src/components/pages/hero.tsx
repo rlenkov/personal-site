58bf1c502398624f9116b5bb9549859798762ee7
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-02-21 18:44:18 +0100

    Create hero page base

--- src/components/pages/hero.tsx
+++ src/components/pages/hero.tsx
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Img from 'gatsby-image'
 import heroStyles from './hero.module.scss'
+import { useStaticQuery, graphql } from 'gatsby'
 import Welcome from './welcome'
 import ScrollContext from '../../context/scrollContext'
 import {
@@ -9,7 +11,42 @@ import {
 
 const Hero = () => {
     const scrollY = useContext(ScrollContext)
+    const [titleIndex, setTitleIndex] = useState(0)
+    const [titles, setTitles] = useState([
+        'software developer',
+        'devOps engineer',
+        'frontend dev',
+        'backend dev',
+        'quasi-physicist',
+        'fiction-writer',
+        'frenchie dad',
+    ])
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * titles.length)
+            setTitleIndex(randomIndex)
+        }, 4000)
+        return () => {
+            clearInterval(timer)
+        }
+    }, [])
+
+    const data = useStaticQuery(graphql`
+        query {
+            contentfulAsset(
+                id: { eq: "a14cac87-c59e-56fb-9e76-4babb241f423" }
+            ) {
+                title
+                fixed(width: 300) {
+                    width
+                    height
+                    src
+                    srcSet
+                }
+            }
+        }
+    `)
     const animatedElements = {
         runOpeningAnimation: {
             id: '',
@@ -19,10 +56,45 @@ const Hero = () => {
 
     const animationStates = useAnimationManager(animatedElements, scrollY)
 
+    const getSlidingTitles = () => {
+        const slide = {
+            transform: `translateX(calc(-${titleIndex} * 400px))`,
+        }
+        return (
+            <div style={slide} className={heroStyles.slidingTitles}>
+                {titles.map(text => {
+                    return (
+                        <span className={heroStyles.changelingText}>
+                            {text}
+                        </span>
+                    )
+                })}
+            </div>
+        )
+    }
+
     return (
         <section className={heroStyles.heroContainer}>
             <Welcome runOpen={animationStates.runOpeningAnimation} />
-            <h1>Hero part</h1>
+            <div className={heroStyles.heroContentBox}>
+                <div className={heroStyles.heroTextBox}>
+                    <h1>Hi There!</h1>
+                    <h2>
+                        I'm Richard a{' '}
+                        <div className={heroStyles.changelingTextBox}>
+                            {getSlidingTitles()}
+                        </div>
+                        ,
+                    </h2>
+                    <h2>ready to get job done for you!</h2>
+                </div>
+                <div className={heroStyles.heroImageBox}>
+                    <Img
+                        className={heroStyles.profileImage}
+                        fixed={data.contentfulAsset.fixed}
+                    />
+                </div>
+            </div>
         </section>
     )
 }

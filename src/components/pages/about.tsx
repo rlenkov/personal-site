8278d409db322b83001dd4f4b37f3ad7f9a10974
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-03-10 10:04:42 +0100

    Create basic structure for about page

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -3,7 +3,17 @@ import aboutStyles from './about.module.scss'
 
 const About = () => (
     <section id="about" className={aboutStyles.aboutContainer}>
-        <h1>About part</h1>
+        <div className={aboutStyles.aboutHeadline}>
+            <h1>Meet The Fixer...</h1>
+        </div>
+        <div className={aboutStyles.aboutContentBox}>
+            <div className={aboutStyles.skillCollections}>
+                <p>Javascript, Javascript, Javascript</p>
+            </div>
+            <div className={aboutStyles.aboutImageBox}>
+                <p>image here</p>
+            </div>
+        </div>
     </section>
 )
 

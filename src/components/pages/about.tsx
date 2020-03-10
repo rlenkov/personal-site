af02287b82c1514937f52358c5d6fdf580018e3d
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-03-10 10:27:14 +0100

    Detail about page structure sketch

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -1,20 +1,66 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from 'gatsby'
 import aboutStyles from './about.module.scss'
 
-const About = () => (
-    <section id="about" className={aboutStyles.aboutContainer}>
-        <div className={aboutStyles.aboutHeadline}>
-            <h1>Meet The Fixer...</h1>
-        </div>
-        <div className={aboutStyles.aboutContentBox}>
-            <div className={aboutStyles.skillCollections}>
-                <p>Javascript, Javascript, Javascript</p>
+const About = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            contentfulAsset(
+                id: { eq: "a14cac87-c59e-56fb-9e76-4babb241f423" }
+            ) {
+                title
+                fixed(width: 300) {
+                    width
+                    height
+                    src
+                    srcSet
+                }
+            }
+        }
+    `)
+    const getSkillsBox = () => {
+        const skills = [
+            'JAVASCRIPT',
+            'HTML',
+            'CSS',
+            'SASS',
+            'REACT',
+            'AWS',
+            'JAVA',
+            'GROOVY',
+            'PYTHON',
+            'PERL',
+        ]
+        const skillSpans = skills.map(skill => {
+            return (
+                <React.Fragment key={`key-skill-tables-${skill}`}>
+                    <span className={aboutStyles.skill}>{skill}</span>
+                    <span className={aboutStyles.separator}>|</span>
+                </React.Fragment>
+            )
+        })
+        return <h3>{skillSpans}</h3>
+    }
+    return (
+        <section id="about" className={aboutStyles.aboutContainer}>
+            <div className={aboutStyles.aboutHeadline}>
+                <h1>Meet The Fixer...</h1>
             </div>
-            <div className={aboutStyles.aboutImageBox}>
-                <p>image here</p>
+            <div className={aboutStyles.aboutContentBox}>
+                <div className={aboutStyles.skillCollections}>
+                    <h4>skills</h4>
+                    {getSkillsBox()}
+                </div>
+                <div className={aboutStyles.aboutImageBox}>
+                    <Img
+                        className={aboutStyles.profileImage}
+                        fixed={data.contentfulAsset.fixed}
+                    />
+                </div>
             </div>
-        </div>
-    </section>
-)
+        </section>
+    )
+}
 
 export default About

8a72a9860b8ba82adbd9db2a41b37b36d01c0406
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-03-10 10:37:39 +0100

    Align footer colors with style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,9 @@ import footerStyles from './footer.module.scss'
 
 const Footer = props => (
     <footer className={footerStyles.footer}>
-        <div>
-            <h1>Bye</h1>
+        <div className={footerStyles.footerBox}>
+            <p>Richard Lenkovits</p>
+            <p>Copyright 2020</p>
         </div>
     </footer>
 )

f8a7bb939fab87890ea3eab584a5608fe2909192
Richard Lenkovits <richard.lenkovits@gmail.com>
2020-03-13 19:49:15 +0100

    Add details to contact page

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -6,12 +6,12 @@ const Contact = () => {
     const scrollY = useContext(ScrollContext)
 
     const boxScrollTransformA = {
-        transform: `translateY(${150 - (scrollY / 20)}px)`,
+        transform: `translateY(${150 - scrollY / 20}px)`,
         transition: 'transform 0.5s ease',
     }
 
     const boxScrollTransformB = {
-        transform: `translateY(${100 - (scrollY / 10)}px)`,
+        transform: `translateY(${100 - scrollY / 10}px)`,
         transition: 'transform 0.5s ease',
     }
 
@@ -23,12 +23,45 @@ const Contact = () => {
                         Find me onl
                         <span className={contactStyles.colored}>ine</span>
                     </span>
-                    <div className={contactStyles.decorationBoxLarge} style={boxScrollTransformA}/>
+                    <div
+                        className={contactStyles.decorationBoxLarge}
+                        style={boxScrollTransformA}
+                    />
                 </h1>
-                <div className={contactStyles.decorationBoxSmall} style={boxScrollTransformB}/>
+                <div
+                    className={contactStyles.decorationBoxSmall}
+                    style={boxScrollTransformB}
+                />
             </div>
             <div className={contactStyles.contactContentBox}>
-                <p>Facebook, insta, linkedin</p>
+                <p>
+                    Let's have a chat on{' '}
+                    <a
+                        target="_blank"
+                        href="www.linkedin.com/in/richard-lenkovits"
+                    >
+                        LinkedIn
+                    </a>
+                    {' or '}
+                    <a
+                        target="_blank"
+                        href="https://www.facebook.com/richard.lenkovits"
+                    >
+                        Facebook
+                    </a>
+                    .
+                </p>
+                <p>
+                    Check out my blog at {' '}
+                    <a target="_blank" href="https://dev.to/pencillr">
+                        dev.to
+                    </a>
+                    {' or simply '}
+                    <a href="mailto:richard.lenkovits@gmail.com">
+                        write a mail
+                    </a>
+                    .
+                </p>
             </div>
         </section>
     )

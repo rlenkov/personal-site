201f93dbceba48e2d0e857145e0a1f5faa9e935a
Richard Lenkovits <richard.lenkovits@gmail.com>
2022-07-01 15:40:40 +0200

    Test and tryal of styling and design

--- src/components/pages/about.tsx
+++ src/components/pages/about.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 import useMetaData from '../../custom-hooks/useMetaData'
 import ScrollContext from '../../context/scrollContext'
 import {
@@ -16,6 +18,14 @@ const About = () => {
             effect: effects.ON_LOW_REACHED,
         },
     }
+
+    const data = useStaticQuery(graphql`
+        query {
+            imageSharp(fluid: { originalName: { eq: "profile-picture.jpg" } }) {
+                gatsbyImageData(quality: 100)
+            }
+        }
+    `)
 
     const slideBoxNumber = 11
 
@@ -117,6 +127,21 @@ const About = () => {
 
     return (
         <section id="about" className={aboutStyles.aboutContainer}>
+            <div className={aboutStyles.waveContainer}>
+                <svg
+                    id="wave"
+                    style={{ transform: `rotate(180deg)`, transition: `0.3s` }}
+                    viewBox="0 0 1440 270"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        style={{ transform: 'translate(0, 0px)', opacity: '1' }}
+                        fill="#131318"
+                        d="M0,135L24,121.5C48,108,96,81,144,85.5C192,90,240,126,288,117C336,108,384,54,432,54C480,54,528,108,576,112.5C624,117,672,72,720,63C768,54,816,81,864,94.5C912,108,960,108,1008,126C1056,144,1104,180,1152,189C1200,198,1248,180,1296,162C1344,144,1392,126,1440,103.5C1488,81,1536,54,1584,72C1632,90,1680,153,1728,153C1776,153,1824,90,1872,67.5C1920,45,1968,63,2016,81C2064,99,2112,117,2160,108C2208,99,2256,63,2304,72C2352,81,2400,135,2448,166.5C2496,198,2544,207,2592,193.5C2640,180,2688,144,2736,126C2784,108,2832,108,2880,112.5C2928,117,2976,126,3024,121.5C3072,117,3120,99,3168,112.5C3216,126,3264,171,3312,198C3360,225,3408,234,3432,238.5L3456,243L3456,270L3432,270C3408,270,3360,270,3312,270C3264,270,3216,270,3168,270C3120,270,3072,270,3024,270C2976,270,2928,270,2880,270C2832,270,2784,270,2736,270C2688,270,2640,270,2592,270C2544,270,2496,270,2448,270C2400,270,2352,270,2304,270C2256,270,2208,270,2160,270C2112,270,2064,270,2016,270C1968,270,1920,270,1872,270C1824,270,1776,270,1728,270C1680,270,1632,270,1584,270C1536,270,1488,270,1440,270C1392,270,1344,270,1296,270C1248,270,1200,270,1152,270C1104,270,1056,270,1008,270C960,270,912,270,864,270C816,270,768,270,720,270C672,270,624,270,576,270C528,270,480,270,432,270C384,270,336,270,288,270C240,270,192,270,144,270C96,270,48,270,24,270L0,270Z"
+                    ></path>
+                </svg>
+            </div>
             <div id="backgroundText" className={aboutStyles.backgroundTextBox}>
                 {[...Array(slideBoxNumber).keys()].map((num) => (
                     <div className={aboutStyles.floatingBox}>
@@ -136,26 +161,33 @@ const About = () => {
                     </div>
                 ))}
             </div>
-            <div
-                className={
-                    animationStates.runHeaderAnimation
-                        ? aboutStyles.aboutHeadlineClosed
-                        : aboutStyles.aboutHeadline
-                }
-            >
-                <h1>Meet The Fixer...</h1>
-            </div>
-            <div className={aboutStyles.cvBox}>
-                <h4>
-                    Prefer the hard facts?{' '}
-                    <a
-                        href={meta.site.siteMetadata.cvLink}
-                        download
-                        target="_blank"
-                    >
-                        Get my CV!
-                    </a>
-                </h4>
+            <div className={aboutStyles.aboutDescriptionBox}>
+                <div
+                    className={
+                        animationStates.runHeaderAnimation
+                            ? aboutStyles.aboutDescriptionRevealed
+                            : aboutStyles.aboutDescription
+                    }
+                >
+                    <h1>Meet The Fixer...</h1>
+                    <GatsbyImage
+                        image={data.imageSharp.gatsbyImageData}
+                        alt="any"
+                        className={aboutStyles.profilePic}
+                    />
+                    <div className={aboutStyles.cvBox}>
+                        <h4>
+                            Prefer the hard facts?{' '}
+                            <a
+                                href={meta.site.siteMetadata.cvLink}
+                                download
+                                target="_blank"
+                            >
+                                Get my CV!
+                            </a>
+                        </h4>
+                    </div>
+                </div>
             </div>
         </section>
     )
--- src/components/pages/work.tsx
+++ src/components/pages/work.tsx
@@ -12,8 +12,20 @@ import * as workStyles from './work.module.scss'
 const Work = () => {
     const scrollY = useContext(ScrollContext)
     const animatedElements = {
-        runHeaderAnimation: {
-            id: 'work-section-animated-text',
+        runHeaderAnimation1: {
+            id: 'work-section-animated-text-1',
+            effect: effects.ON_TOP_REACHED,
+        },
+        runHeaderAnimation2: {
+            id: 'work-section-animated-text-2',
+            effect: effects.ON_TOP_REACHED,
+        },
+        runHeaderAnimation3: {
+            id: 'work-section-animated-text-3',
+            effect: effects.ON_TOP_REACHED,
+        },
+        runHeaderAnimation4: {
+            id: 'work-section-animated-text-4',
             effect: effects.ON_TOP_REACHED,
         },
     }
@@ -21,22 +33,50 @@ const Work = () => {
     const animationStates = useAnimationManager(animatedElements, scrollY)
 
     const getMovement = (divider, limit) => {
-        return (scrollY / divider) < limit ? (scrollY / divider) : limit
+        return scrollY / divider < limit ? scrollY / divider : limit
     }
 
     const boxScrollTransformA = {
-        transform: `translateY(${getMovement(20, 80)}px)`,
-        transition: 'transform 0.5s ease',
+        transform: `translateY(${getMovement(15, 200)}px)`,
+        transition: 'transform 0.5s ease-out',
     }
 
     const boxScrollTransformB = {
-        transform: `translateY(${getMovement(10, 200)}px)`,
-        transition: 'transform 0.5s ease',
+        transform: `translateY(${getMovement(8, 250)}px)`,
+        transition: 'transform 0.5s ease-out',
+    }
+
+    const boxScrollTransformC = {
+        transform: `translateY(${getMovement(
+            15,
+            200
+        )}px) translateX(${getMovement(2, 500)}px)`,
+        transition: 'transform 0.5s ease-out',
+    }
+
+    const boxScrollTransformD = {
+        transform: `translateY(${getMovement(
+            8,
+            250
+        )}px) translateX(${getMovement(2, 500)}px)`,
+        transition: 'transform 0.7s ease-out',
+    }
+
+    const boxScrollTransformE = {
+        transform: `translateY(${getMovement(
+            1,
+            300
+        )}px) translateX(${getMovement(10, 200)}px)`,
+        transition: 'transform 0.7s ease-out',
     }
 
     return (
         <section id="work" className={workStyles.workContainer}>
             <div className={workStyles.svgBox}>
+                <Portal
+                    className={workStyles.svgPortal}
+                    style={boxScrollTransformB}
+                />
                 <MachineBot
                     className={workStyles.svgBot}
                     style={boxScrollTransformB}
@@ -45,23 +85,47 @@ const Work = () => {
                     className={workStyles.svgTop}
                     style={boxScrollTransformA}
                 />
-                <Portal
-                    className={workStyles.svgPortal}
-                    style={boxScrollTransformB}
-                />
             </div>
             <div className={workStyles.workContentBox}>
                 <div
-                    className={
-                        animationStates.runHeaderAnimation
-                            ? workStyles.textBlockClosed
-                            : workStyles.textBlock
-                    }
-                    id="work-section-animated-text"
+                    className={[
+                        workStyles.textBlockEdit,
+                        workStyles.blockA,
+                    ].join(' ')}
+                    id="work-section-animated-text-1"
+                    style={boxScrollTransformC}
                 >
-                    <h1>The things</h1>
-                    <h1>we could</h1>
-                    <h1>create..!?</h1>
+                    <h1>Need frontend?</h1>
+                </div>
+                <div
+                    className={[
+                        workStyles.textBlockEdit,
+                        workStyles.blockB,
+                    ].join(' ')}
+                    id="work-section-animated-text-2"
+                    style={boxScrollTransformD}
+                >
+                    <h1>Backend?</h1>
+                </div>
+                <div
+                    className={[
+                        workStyles.textBlockEdit,
+                        workStyles.blockC,
+                    ].join(' ')}
+                    id="work-section-animated-text-3"
+                    style={boxScrollTransformE}
+                >
+                    <h1>or DevOps?</h1>
+                </div>
+                <div
+                    className={[
+                        workStyles.textBlockEdit,
+                        workStyles.blockD,
+                    ].join(' ')}
+                    id="work-section-animated-text-4"
+                    style={boxScrollTransformB}
+                >
+                    <h1>Imagination's the limit!</h1>
                 </div>
             </div>
         </section>
